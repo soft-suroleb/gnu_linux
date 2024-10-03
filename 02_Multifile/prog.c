dabbeb6cb489456d101e1e1490f80a13a23ad827
Pavel Bozin <bozin2002@yandex.ru>
2024-10-03 23:11:23 +0300

    comparing three bins

--- 02_Multifile/prog.c
+++ 02_Multifile/prog.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "outlib.h"
 int main(int argc, char *argv[]) {
-  int i;
+	int i;
         if((Count = argc)>1) {
-                output("<INIT>");
-             for(i=1; i<argc; i++)
-                 output(argv[i]);
-              output("<DONE>");
-     }
-     else
-          usage(argv[0]);
-       return 0;
+        	output("<INIT>");
+        	for(i=1; i<argc; i++)
+			output(argv[i]);
+              	output("<DONE>");
+     } else
+	usage("prog");
+	
+	return 0;
 }
