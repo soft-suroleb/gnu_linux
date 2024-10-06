51fc2d083bf22b1f542800cc7fa7ecb6bf8de653
Pavel Bozin <bozin2002@yandex.ru>
2024-10-06 20:00:19 +0300

    little fixes 3 hw

--- 03_Text/randomizer.sh
+++ 03_Text/randomizer.sh
@@ -6,8 +6,7 @@ TMPFILE=$(mktemp)
 TMPFILE2=$(mktemp)
 echo $TMPFILE
 
-max_len=0
-current=0
+current_row=0
 delay=$1
 
 while read -r line; do
@@ -15,17 +14,12 @@ while read -r line; do
 		letter=${line:$i:1}
 		if [[ ! $letter = *[[:space:]]* ]]
 		then
-			echo $letter $current $i >> $TMPFILE
+			echo $letter $current_row $i >> $TMPFILE
 		fi	
 	done
 
-	if (($i>$max_len))
-	then
-		max_len=$i
-	fi;
-
-	current=$((current + 1))
-done;
+	current_row=$((current_row + 1))
+done
 
 shuf < $TMPFILE -o $TMPFILE
 clear
@@ -34,13 +28,13 @@ while read -r line; do
 	# вот этот костыль нужен, потому что у меня почему-то не работает
 	# обычный пайп echo $line | read -r letter row col
 	# просто в консоли работает а здесь нет, с чем связано не понимаю
-	echo $line > TMPFILE2
-	IFS=" " read -r letter row col < TMPFILE2
+	echo $line > $TMPFILE2
+	IFS=" " read -r letter row col < $TMPFILE2
 	tput cup $row $col
 	echo $letter
 	sleep $delay
 done < $TMPFILE
-tput cup $((current)) 0
+tput cup $((current_row)) 0
 
 rm -f $TMPFILE
 rm -f $TMPFILE2
